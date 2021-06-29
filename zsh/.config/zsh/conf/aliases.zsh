51451f1ba9bc9cf163a8196b696af4f9ecdd0127
Zahin Muhaimeen <zahin.muhaimeen@gmail.com>
2021-06-29 16:20:32 +1000

    zsh: check if an alias can work

--- zsh/.config/zsh/conf/aliases.zsh
+++ zsh/.config/zsh/conf/aliases.zsh
@@ -1,21 +1,29 @@
-alias ls='exa -a --color=always --sort=type'
+if pacman -Qs exa > /dev/null ; then
+    alias ls='exa -a --color=always --sort=type'
+else
+    alias ls='ls -a --color --sort=version'
+fi
 alias rls="clear ; ls"
-alias grep='rg'
-alias cat='bat'
+if pacman -Qs ripgrep > /dev/null ; then
+    alias grep='rg'
+fi
 alias rm="rm -i"
 alias mkdir="mkdir -p"
 
 ##  GIT  ##
 
-alias gs='git status'
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gp='git push'
-alias ga='git add'
-alias gb='git checkout'
+if pacman -Qs git > /dev/null ; then
+    alias gs='git status'
+    alias gc='git commit'
+    alias gcm='git commit -m'
+    alias gp='git push'
+    alias ga='git add'
+    alias gb='git checkout'
+fi
 
 ##  NVIM TYPING ERRORS  ##
 
 alias nimv="nvim"
 alias nivm="nvim"
 alias vnim="nivm"
+
