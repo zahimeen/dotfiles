53eb6106ba1fe5a45692db8a28870c91adaf6b0e
zahin-muhaimeen <zahin.muhaimeen@gmail.com>
2021-07-29 20:08:37 +1000

    removed neovim, lvim and macchina configs

--- nvim/.config/nvim/ftplugin/yaml.lua
+++
@@ -1,8 +0,0 @@
-require("lspconfig").yamlls.setup({
-    cmd = { DATA_PATH .. "/lspinstall/yaml/node_modules/.bin/yaml-language-server", "--stdio" },
-    on_attach = require("lsp").common_on_attach,
-})
-
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
--- nvim/.config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("settings")
-require("plugin_list")
-require("highlights")
