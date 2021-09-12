8c4e1e627efcaa4d219ac7d106aa9d599480f98c
zahin-muhaimeen <zahin.muhaimeen@gmail.com>
2021-09-12 17:41:04 +1000

    lvim: dashboard gray; sway: borders are back

--- .config/lvim/lua/user/aesthetics.lua
+++ .config/lvim/lua/user/aesthetics.lua
@@ -1,2 +1,11 @@
--- vim.g.guicursor = "a:a"
 lvim.colorscheme = "onedarker"
+
+vim.cmd("autocmd FileType dashboard lua require('user.aesthetics').hl_dashboard()")
+
+local M = {}
+M.hl_dashboard = function ()
+    vim.cmd("highlight DashboardHeader guifg=#5c6370")
+    vim.cmd("highlight DashboardCenter guifg=#5c6370")
+    vim.cmd("highlight DashboardFooter guifg=#5c6370")
+end
+return M
--- .config/lvim/lua/user/settings.lua
+++ .config/lvim/lua/user/settings.lua
@@ -1,8 +1,9 @@
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.colorcolumn = "80"
 vim.opt.hlsearch = false
+-- vim.opt.guicursor = "a:a"
