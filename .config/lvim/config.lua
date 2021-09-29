33339d8cba8093447449208b74f78d78e689d300
zahin-muhaimeen <zahin.muhaimeen@gmail.com>
2021-09-29 14:07:44 +1000

    using prettierd now because it is much faster

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,6 +2,8 @@ lvim.format_on_save = true
 lvim.lint_on_save = true
 lvim.log.level = "warn"
 lvim.lsp.diagnostics.virtual_text = false
+lvim.builtin.cmp.documentation.border = { " ", " ", " ", " ", " ", " ", " ", " " }
+lvim.builtin.lualine.sections.lualine_z = {}
 
 require("user.settings")
 require("user.aesthetics")
--- .profile
+++ .profile
@@ -1,5 +1,7 @@
+export PATH=$HOME/.local/bin:$PATH
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
-export PATH=$HOME/.local/bin:$PATH
+
+export PRETTIERD_DEFAULT_CONFIG=$HOME/.config/prettier/.prettierrc.json
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
