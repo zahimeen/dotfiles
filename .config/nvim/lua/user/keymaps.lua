592d13614f52f57e15768a10a14b4644dba497be
Zahin Al Muhaimeen <zahin.muhaimeen@gmail.com>
2024-04-18 21:58:30 +1000

    nvim: sourcing file keymap stuff

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -13,14 +13,48 @@ map("n", "<esc>", "<cmd>noh<cr>")
 
 map("n", "<leader>wv", "<C-w>v")
 map("n", "<leader>ws", "<C-w>s")
-map({ "n", "v", "i" }, "<A-h>", "<C-w>h")
-map({ "n", "v", "i" }, "<A-l>", "<C-w>l")
-map({ "n", "v", "i" }, "<A-j>", "<C-w>j")
-map({ "n", "v", "i" }, "<A-k>", "<C-w>k")
+-- prolly already managed by tmux keymaps
+-- map({ "n", "v", "i", "t" }, "<A-h>", "<C-w>h")
+-- map({ "n", "v", "i", "t" }, "<A-l>", "<C-w>l")
+-- map({ "n", "v", "i", "t" }, "<A-j>", "<C-w>j")
+-- map({ "n", "v", "i", "t" }, "<A-k>", "<C-w>k")
 
 -- terminal escape
 map("t", "<C-;>", "<C-\\><C-n>") -- FIXME: its not working idk why
 
 -- you can toggle an opt with !
-map("n", "<leader>so", "<cmd>set sc!<cr>")
+map("n", "<leader>ke", "<cmd>set sc!<cr>")
 map("n", "<leader>rn", "<cmd>set nu! rnu!<cr>")
+
+map("n", "<leader>co", function()
+	-- some freaky nvim bs don worry bout it lil bro
+	---@diagnostic disable-next-line: undefined-field
+	if vim.opt.colorcolumn._value == "" then
+		vim.opt.colorcolumn = "80"
+		return
+	end
+	vim.opt.colorcolumn = ""
+end)
+
+map("n", "<leader>so", function()
+	local directory = vim.fn.expand("~/dotfiles/.config/nvim/lua/")
+	local filepath = vim.fn.expand("%:p")
+
+	if not (vim.opt_local.filetype._value == "lua") or not (filepath:sub(1, #directory) == directory) then
+		vim.cmd.source()
+		print("Sourced current file!")
+		return
+	end
+
+	local modname = filepath:sub(#directory + 1):gsub("%..-$", ""):gsub("/", ".")
+
+	package.loaded[modname] = nil
+	local returns = require(modname)
+	if type(returns) == "table" and type(returns.config) == "function" then
+		returns.config()
+		print("Sourced current file and ran config function!")
+		return
+	end
+
+	print("Sourced current file, but no config function!")
+end)
--- .config/nvim/lua/user/plugins/tmux.lua
+++ .config/nvim/lua/user/plugins/tmux.lua
@@ -12,15 +12,15 @@ M.config = function()
 	})
 
 	local map = vim.keymap.set
-	map({ "n", "v", "i" }, "<A-h>", tmux.move_left)
-	map({ "n", "v", "i" }, "<A-l>", tmux.move_right)
-	map({ "n", "v", "i" }, "<A-j>", tmux.move_bottom)
-	map({ "n", "v", "i" }, "<A-k>", tmux.move_top)
+	map({ "n", "v", "i", "t" }, "<A-h>", tmux.move_left)
+	map({ "n", "v", "i", "t" }, "<A-l>", tmux.move_right)
+	map({ "n", "v", "i", "t" }, "<A-j>", tmux.move_bottom)
+	map({ "n", "v", "i", "t" }, "<A-k>", tmux.move_top)
 
-	map({ "n", "v", "i" }, "<C-h>", tmux.resize_left)
-	map({ "n", "v", "i" }, "<C-l>", tmux.resize_right)
-	map({ "n", "v", "i" }, "<C-j>", tmux.resize_bottom)
-	map({ "n", "v", "i" }, "<C-k>", tmux.resize_top)
+	map({ "n", "v", "i", "t" }, "<C-h>", tmux.resize_left)
+	map({ "n", "v", "i", "t" }, "<C-l>", tmux.resize_right)
+	map({ "n", "v", "i", "t" }, "<C-j>", tmux.resize_bottom)
+	map({ "n", "v", "i", "t" }, "<C-k>", tmux.resize_top)
 end
 
 return M
