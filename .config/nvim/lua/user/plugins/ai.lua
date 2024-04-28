6890cb4f6fef8182f2549cf382f4f68920482bd8
Zahin Al Muhaimeen <zahin.muhaimeen@gmail.com>
2024-04-28 12:37:14 +1000

    nvim: added codeium plugin

---
+++ .config/nvim/lua/user/plugins/ai.lua
@@ -0,0 +1,21 @@
+local M = {
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
+}
+
+M.config = function()
+	vim.keymap.set("i", "<C-g>", function()
+		return vim.fn["codeium#Accept"]()
+	end, { expr = true, silent = true })
+	vim.keymap.set("i", "<c-;>", function()
+		return vim.fn["codeium#CycleCompletions"](1)
+	end, { expr = true, silent = true })
+	vim.keymap.set("i", "<c-,>", function()
+		return vim.fn["codeium#CycleCompletions"](-1)
+	end, { expr = true, silent = true })
+	vim.keymap.set("i", "<c-x>", function()
+		return vim.fn["codeium#Clear"]()
+	end, { expr = true, silent = true })
+end
+
+return M
